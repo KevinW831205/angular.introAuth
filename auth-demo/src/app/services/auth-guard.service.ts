5fd9757b4fd95c15a91f5cb1d5f2319a47273337
KevinW <bird831205@hotmail.com>
2019-12-16 10:18:38 -0500

    implementing authguard now redirects to login page

--- auth-demo/src/app/services/auth-guard.service.ts
+++ auth-demo/src/app/services/auth-guard.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate{
 
-  constructor() { }
+  constructor(private router: Router ,private authService: AuthService) { }
+
+  canActivate(){
+    if(this.authService.isLoggedIn()){
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
+
+  }
 
 }
