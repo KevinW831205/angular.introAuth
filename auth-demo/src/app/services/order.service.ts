dfef09a4c6a26d23230f3625b6d581b5ae9d4651
KevinW <bird831205@hotmail.com>
2019-12-16 10:41:56 -0500

    using jwt built in authhttp

--- auth-demo/src/app/services/order.service.ts
+++ auth-demo/src/app/services/order.service.ts
@@ -1,22 +1,24 @@
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import {AuthHttp} from 'angular2-jwt'
 
 @Injectable()
 export class OrderService {
 
-  constructor(private http: Http) {
+  constructor(private AuthHttp: AuthHttp, http: Http) {
   }
 
   getOrders() { 
 
-    let headers = new Headers();
-    let token = localStorage.getItem('token')
-    headers.append('Authorization','Bearer '+token);
+    // let headers = new Headers();
+    // let token = localStorage.getItem('token')
+    // headers.append('Authorization','Bearer '+token);
 
-    let options = new RequestOptions({headers: headers});
+    // let options = new RequestOptions({headers: headers});
 
-    return this.http.get('/api/orders', options)
+    // return this.http.get('/api/orders', options)
+    return this.AuthHttp.get('/api/orders')
       .map(response => response.json());
   }
 }
